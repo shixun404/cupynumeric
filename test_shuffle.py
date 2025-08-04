ba4f805d8de26fbb4ee29fbc2fabf78fc649efb2
Shixun Wu <shixunw@lego-c2-qs-60.cm.cluster>
2025-08-04 00:03:54 -0700

    update

--- test_shuffle.py
+++ test_shuffle.py
@@ -1,17 +1,13 @@
 import cupynumeric as np
 import numpy
-d1 = 10
+d1 = 100000000
 d2 = 2
 input = numpy.arange(d1 * d2)
 input = input.reshape(d1, d2)
 a = np.array(input)
-print(type(a[0, 0]))
-print(type(a))
+
 print(a)
+
 a.shuffle()
+
 print(a)
-
-
-# input.sort()
-# print(input[:5])
-

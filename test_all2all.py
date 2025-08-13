6af4cc48df52398574260bbef38dff68e7172370
Shixun Wu <shixunw@lego-c2-qs-60.cm.cluster>
2025-08-13 13:48:54 -0700

    update

--- test_all2all.py
+++ test_all2all.py
@@ -2,13 +2,13 @@ import cupynumeric as np
 import numpy
 
 # input = numpy.array([0,1,2,3,4,5,6,7,8,9])
-input = numpy.arange(32)
+input = numpy.arange(8)
 # input = input.reshape(10, 10)
 
 a = np.array(input)
 print(a)
 # b = a[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]]
-b = a[[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]]
+b = a[[0,1,2,3]]
 # b = a[[1,2,3,4]]
 print(b)
 

9b4f68609c0dadd25695295378122ecef053b6f1
Shixun Wu <shixunw@lego-c2-qs-60.cm.cluster>
2025-08-14 14:17:53 -0700

    update

--- test_fancy_indexing.py
+++ test_fancy_indexing.py
@@ -17,7 +17,7 @@ data_array = np.arange(N_ROWS * N_COLS, dtype=np.float32)
 print("Data generated")
 
 print("Taking pairs")
-pair_list = [i * N_COLS + j for i in range(N_ROWS) for j in range(N_COLS)]
+pair_list = [i * N_COLS + j for i in range(N_ROWS) for j in range(N_COLS) for  k in range(16)]
 pairs_array = np.array(pair_list)
 print(len(pair_list))
 

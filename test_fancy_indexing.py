9cb34014df5e801956ee6b023f53a2ed21591ec7
Shixun Wu <shixunw@lego-c2-qs-60.cm.cluster>
2025-08-14 13:48:50 -0700

    update

--- test_fancy_indexing.py
+++ test_fancy_indexing.py
@@ -12,11 +12,12 @@ def take_with_pairs(data, pairs):
     return np.take(data, pairs, axis=1)
 
 
-data_array = np.arange(N_ROWS * N_COLS, dtype=np.float32).reshape((N_ROWS, N_COLS))
+data_array = np.arange(N_ROWS * N_COLS, dtype=np.float32)
+# .reshape((N_ROWS, N_COLS))
 print("Data generated")
 
 print("Taking pairs")
-pair_list = [[i, j] for i in range(N_COLS) for j in range(i + 1, N_COLS)]
+pair_list = [[i * N_COLS + j] for i in range(N_COLS) for j in range(i + 1, N_COLS)]
 pairs_array = np.array(pair_list)
 print(len(pair_list))
 

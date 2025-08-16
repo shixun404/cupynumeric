4ee609b9df45888c71051273fa1292f2546eb416
Shixun Wu <shixunw@lego-c2-qs-60.cm.cluster>
2025-08-15 23:55:54 -0700

    update

--- run_2.sh
+++ run_2.sh
@@ -17,6 +17,9 @@ conda activate legate
 # export CONDA_ENV_DIR="/project/coreai_devtech_all/shixunw/miniconda3/envs/legate"
 # export PATH="$CONDA_ENV_DIR/bin:$PATH"
 # 在 run_2.sh 中添加
+export OMPI_MCA_btl_tcp_if_include=eth0  # 或者你的网络接口名
+export OMPI_MCA_btl=tcp,self,vader
+export OMPI_MCA_pml=ob1
 export OMPI_MCA_btl_base_verbose=100  # 最详细的 BTL 调试
 export OMPI_MCA_mca_base_component_show_load_errors=1  # 显示组件加载错误
 export HOME=/project/coreai_devtech_all/shixunw;

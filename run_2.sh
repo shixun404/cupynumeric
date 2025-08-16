3ea1160d789fc28e3b6454773ac6b8506b48e5f7
Shixun Wu <shixunw@lego-c2-qs-60.cm.cluster>
2025-08-15 17:18:21 -0700

    update

--- run_2.sh
+++ run_2.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 # --- Core Software Paths ---
+__conda_setup="$('/project/coreai_devtech_all/shixunw/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+   eval "$__conda_setup"
+else
+   if [ -f "/project/coreai_devtech_all/shixunw/miniconda3/etc/profile.d/conda.sh" ]; then
+       . "/project/coreai_devtech_all/shixunw/miniconda3/etc/profile.d/conda.sh"
+   else
+       export PATH="/project/coreai_devtech_all/shixunw/miniconda3/bin:$PATH"
+   fi
+fi
+unset __conda_setup
+conda activate legate
+
 export CONDA_ENV_DIR="/project/coreai_devtech_all/shixunw/miniconda3/envs/legate"
 export PATH="$CONDA_ENV_DIR/bin:$PATH"
 export HOME=/project/coreai_devtech_all/shixunw;
@@ -9,8 +22,6 @@ export HPC_SDK_ROOT=/opt/nvidia/hpc_sdk/Linux_x86_64/25.3;
 export OPAL_PREFIX=$HPC_SDK_ROOT/comm_libs/12.8/hpcx/hpcx-2.22.1/ompi;
 export UCX_HOME=$HPC_SDK_ROOT/comm_libs/12.8/hpcx/hpcx-2.22.1/ucx
 export UCX_MODULE_DIR=$UCX_HOME/lib/ucx
-
-
 export UCX_LIB_DIR="$HPC_SDK_ROOT/comm_libs/12.8/hpcx/hpcx-2.22.1/ucx/lib"
 export MPI_LIB_DIR="$HPC_SDK_ROOT/comm_libs/12.8/hpcx/hpcx-2.22.1/ompi/lib"
 export LD_LIBRARY_PATH="$UCX_HOME/lib:$MPI_HOME/lib"
@@ -19,8 +30,6 @@ export HDF5_LIBRARIES=/root/hdf5-1.14.3/hdf5/lib;
 export HDF5_ROOT=/root/hdf5-1.14.3/hdf5;
 export HDF5_DIR=/root/hdf5-1.14.3/config/cmake;
 export CMAKE_PREFIX_PATH=$HDF5_ROOT:$CMAKE_PREFIX_PATH;
-
-
 export LD_LIBRARY_PATH=$HDF5_ROOT/lib:$LD_LIBRARY_PATH;
 export LD_LIBRARY_PATH=$HPC_SDK_ROOT/comm_libs/12.8/hpcx/hpcx-2.22.1/ucc/lib:$LD_LIBRARY_PATH;
 export LD_LIBRARY_PATH=$HPC_SDK_ROOT/comm_libs/12.8/hpcx/latest/ucc/lib:$LD_LIBRARY_PATH;
@@ -30,8 +39,6 @@ export LD_LIBRARY_PATH=$MPI_HOME/lib:$LD_LIBRARY_PATH;
 export LD_LIBRARY_PATH=$HPC_SDK_ROOT/compilers/lib:$LD_LIBRARY_PATH;
 export LD_LIBRARY_PATH=$MPI_HOME/lib:$LD_LIBRARY_PATH;
 export LD_PRELOAD="$UCX_LIB_DIR/libucs.so.0:$UCX_LIB_DIR/libucp.so.0:$UCX_LIB_DIR/libucm.so.0:$MPI_LIB_DIR/libmpi.so"
-
-
 export PATH=$HPC_SDK_ROOT/compilers/bin:$PATH;
 export PATH=$MPI_HOME/bin:$PATH;
 
@@ -60,9 +67,7 @@ export CPATH=$CUDNN_PATH/include:$CPATH;
 export RAPIDS_LIBUCX_PREFER_SYSTEM_LIBRARY=1;
 export OMPI_ALLOW_RUN_AS_ROOT=1
 export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
-#export LD_LIBRARY_PATH=$HOME/miniconda3/envs/legate_1/lib:$LD_LIBRARY_PATH;
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$CONDA_ENV_DIR/lib"
-export LD_LIBRARY_PATH=$HOME/miniconda3/envs/legate_1/lib/libibverbs:$LD_LIBRARY_PATH;
+
 # export OMPI_MCA_btl_base_verbose=100
 # NEW: Explicitly tell the UCX CUDA transport to avoid gdrcopy
 #export UCX_CUDA_TLS=ipc,cma

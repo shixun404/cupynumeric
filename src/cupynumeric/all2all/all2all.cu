0f825170889c0e312d9b6a8b7f6a7f2c5a5427d4
Shixun Wu <shixunw@lego-c2-qs-60.cm.cluster>
2025-08-22 10:01:07 -0700

    update

--- src/cupynumeric/all2all/all2all.cu
+++ src/cupynumeric/all2all/all2all.cu
@@ -271,29 +271,29 @@ void global_all2all(
   CHECK_NCCL(ncclGroupEnd());
   size_t total_indices_to_receive = thrust::reduce(round1_recv_histo.begin(), round1_recv_histo.end());
     
-  // thrust::device_vector<legate::Point<DIM_input>> round2_recv_indices(total_indices_to_receive);
-  // thrust::exclusive_scan(round1_recv_histo.begin(), round1_recv_histo.end(), round1_recv_offsets.begin());
+  thrust::device_vector<legate::Point<DIM_input>> round2_recv_indices(total_indices_to_receive);
+  thrust::exclusive_scan(round1_recv_histo.begin(), round1_recv_histo.end(), round1_recv_offsets.begin());
 
-  // cudaStreamSynchronize(stream);
-  // // // ===== Round 2: All2All exchange request indices =====
-  // CHECK_NCCL(ncclGroupStart());
-  // for (size_t i = 0; i < num_ranks; ++i) {
-  //     unsigned int indices_to_send_to_rank_i = round1_send_histo[i];
-  //     unsigned int send_offset_for_rank_i = round1_send_offsets[i];
-  //     unsigned int indices_to_recv_from_rank_i = round1_recv_histo[i];
-  //     unsigned int recv_offset_for_rank_i = round1_recv_offsets[i];
-  //     if (indices_to_send_to_rank_i > 0) {
-  //         CHECK_NCCL(ncclSend(thrust::raw_pointer_cast(round2_send_indices.data()) + send_offset_for_rank_i * DIM_input,
-  //           indices_to_send_to_rank_i * sizeof(legate::Point<DIM_input>), ncclInt8, i, *nccl_comm, stream));
-  //     }
+  cudaStreamSynchronize(stream);
+  // // ===== Round 2: All2All exchange request indices =====
+  CHECK_NCCL(ncclGroupStart());
+  for (size_t i = 0; i < num_ranks; ++i) {
+      unsigned int indices_to_send_to_rank_i = round1_send_histo[i];
+      unsigned int send_offset_for_rank_i = round1_send_offsets[i];
+      unsigned int indices_to_recv_from_rank_i = round1_recv_histo[i];
+      unsigned int recv_offset_for_rank_i = round1_recv_offsets[i];
+      if (indices_to_send_to_rank_i > 0) {
+          CHECK_NCCL(ncclSend(thrust::raw_pointer_cast(round2_send_indices.data()) + send_offset_for_rank_i * DIM_input,
+            indices_to_send_to_rank_i * sizeof(legate::Point<DIM_input>), ncclInt8, i, *nccl_comm, stream));
+      }
       
-  //     if (indices_to_recv_from_rank_i > 0) {
-  //         CHECK_NCCL(ncclRecv(thrust::raw_pointer_cast(round2_recv_indices.data()) + recv_offset_for_rank_i * DIM_input,
-  //                 indices_to_recv_from_rank_i * sizeof(legate::Point<DIM_input>), ncclInt8, i, *nccl_comm, stream));
-  //     }
-  // }
-  // CHECK_NCCL(ncclGroupEnd());
-  // cudaStreamSynchronize(stream);
+      if (indices_to_recv_from_rank_i > 0) {
+          CHECK_NCCL(ncclRecv(thrust::raw_pointer_cast(round2_recv_indices.data()) + recv_offset_for_rank_i * DIM_input,
+                  indices_to_recv_from_rank_i * sizeof(legate::Point<DIM_input>), ncclInt8, i, *nccl_comm, stream));
+      }
+  }
+  CHECK_NCCL(ncclGroupEnd());
+  cudaStreamSynchronize(stream);
 
   // thrust::device_vector<DataType> round3_send_data(total_indices_to_receive);
   

3373d00e0b55adfdd2c32989f4318be04ae9a686
Shixun Wu <shixunw@lego-c2-qs-60.cm.cluster>
2025-08-21 17:22:53 -0700

    update

--- src/cupynumeric/all2all/all2all.cu
+++ src/cupynumeric/all2all/all2all.cu
@@ -184,7 +184,7 @@ int device_id;
 cudaGetDevice(&device_id);
 cudaDeviceProp prop;
 cudaGetDeviceProperties(&prop, device_id);
-printf("Rank %d is using GPU device %d (%s)\n", rank_id, device_id, prop.name);
+// printf("Rank %d is using GPU device %d (%s)\n", rank_id, device_id, prop.name);
 
   size_t local_input_count = get_volume<DIM_input>(input_rect);
   size_t local_index_count = get_volume<DIM_output>(index_rect);

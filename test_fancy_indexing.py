e1ddb89db51f847bd000eb01f506d221ebfb7e11
Shixun Wu <shixunw@lego-c2-qs-60.cm.cluster>
2025-08-22 13:01:33 -0700

    update

--- test_fancy_indexing.py
+++ test_fancy_indexing.py
@@ -20,7 +20,7 @@ print("Taking pairs")
 pair_list = [(i * N_COLS + j + k) % N_ROWS for i in range(N_ROWS) for j in range(N_COLS) for  k in range(16)]
 pairs_array = np.array(pair_list)
 print(len(pair_list))
-
+print(data_array.shape)
 start_time = time()
 # result_array = take_with_pairs(data_array, pairs_array)
 result_array = data_array[pairs_array]

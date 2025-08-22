9f5227dacd27c8d5a587a10f0d20b24398011699
Shixun Wu <shixunw@lego-c2-qs-60.cm.cluster>
2025-08-22 16:44:18 -0700

    update

--- cupynumeric/_thunk/deferred.py
+++ cupynumeric/_thunk/deferred.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 #
 from __future__ import annotations
-
+import nvtx
 import weakref
 from collections import Counter
 from collections.abc import Iterable
@@ -982,25 +982,28 @@ class DeferredArray(NumPyThunk):
                 #     result.base, rhs.base, index_array.base  # type: ignore
                 # )
                 # Add communicators if needed for distributed shuffle
-                
-                task = legate_runtime.create_auto_task(
-                    self.library, CuPyNumericOpCode.ALL2ALL
-                )
-                task.add_input(rhs.base)
-                task.add_input(index_array.base)
-                task.add_output(result.base)
-                # Add scalar arguments
-                task.add_scalar_arg(rhs.base.shape, (ty.int64,))   # total volume
-                task.add_scalar_arg(index_array.base.shape, (ty.int64,))   # total volume
-                task.add_scalar_arg(result.base.shape, (ty.int64,))   # total volume
-    
-                
-                if runtime.num_gpus > 1:
-                    task.add_nccl_communicator()
-                elif runtime.num_gpus == 0 and runtime.num_procs > 1:
-                    task.add_cpu_communicator()
+                with nvtx.annotate("add_create_all2all_task", color="red"):
+                    task = legate_runtime.create_auto_task(
+                        self.library, CuPyNumericOpCode.ALL2ALL
+                    )
+                with nvtx.annotate("add_input_output", color="blue"):
+                    task.add_input(rhs.base)
+                    task.add_input(index_array.base)
+                    task.add_output(result.base)
+                with nvtx.annotate("add_scalar_args", color="green"):
+                    # Add scalar arguments
+                    task.add_scalar_arg(rhs.base.shape, (ty.int64,))   # total volume
+                    task.add_scalar_arg(index_array.base.shape, (ty.int64,))   # total volume
+                    task.add_scalar_arg(result.base.shape, (ty.int64,))   # total volume
+        
+                with nvtx.annotate("add_communicators", color="yellow"):
+                    if runtime.num_gpus > 1:
+                        task.add_nccl_communicator()
+                    elif runtime.num_gpus == 0 and runtime.num_procs > 1:
+                        task.add_cpu_communicator()
                     
-                task.execute()
+                with nvtx.annotate("execute", color="purple"):
+                    task.execute()
 
             else:
                 return index_array

d5b0eb27df8827a13de6940ce6398e98acfca957
Shixun Wu <shixunw@lego-c2-qs-60.cm.cluster>
2025-08-23 17:57:36 -0700

    update

--- test_fancy_indexing.py
+++ test_fancy_indexing.py
@@ -28,10 +28,12 @@ result_array = data_array[pairs_array]
 duration = time() - start_time
 print(duration / 1e6)
 
-# start_time = time()
-# result_array = data_array[pairs_array]
-# duration = time() - start_time
-# print(duration / 1e6)
+
+
+start_time = time()
+result_array_1 = data_array[pairs_array]
+duration = time() - start_time
+print(duration / 1e6)
 
 # start_time = time()
 # result_array = data_array[pairs_array]

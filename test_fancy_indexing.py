a89aa8df3f6f742005fc91d46d553c1d7dac1d29
Shixun Wu <shixunw@lego-c2-qs-60.cm.cluster>
2025-08-23 18:19:36 -0700

    update

--- test_fancy_indexing.py
+++ test_fancy_indexing.py
@@ -22,6 +22,7 @@ pair_list = np.arange(N_ROWS * N_COLS, dtype=np.int64)
 pairs_array = np.array(pair_list)
 # print(len(pair_list))
 print(data_array.shape)
+start_time = time()
 for i in range(3):
     result_array = data_array[pairs_array]
 

3b40483e065771a1a0f7e0a0dd0ae99eff9fdccb
Shixun Wu <shixunw@lego-c2-qs-60.cm.cluster>
2025-08-24 13:32:45 -0700

    update

--- test_fancy_indexing.py
+++ test_fancy_indexing.py
@@ -1,39 +1,39 @@
-import cupynumeric as np
-from legate.timing import time
+# import cupynumeric as np
+# from legate.timing import time
 
 
-N_ROWS = 1_000_000
-N_COLS = 64
 # N_ROWS = 1_000_000
-# N_COLS = 32
+# N_COLS = 64
+# # N_ROWS = 1_000_000
+# # N_COLS = 32
 
 
-def take_with_pairs(data, pairs):
-    return np.take(data, pairs, axis=1)
+# def take_with_pairs(data, pairs):
+#     return np.take(data, pairs, axis=1)
 
 
-data_array = np.arange(N_ROWS * N_COLS, dtype=np.float32)
-# .reshape((N_ROWS, N_COLS))
-print("Data generated")
+# data_array = np.arange(N_ROWS * N_COLS, dtype=np.float32)
+# # .reshape((N_ROWS, N_COLS))
+# print("Data generated")
 
-print("Taking pairs")
-# pair_list = [(i * N_COLS + j + k) % N_ROWS for i in range(N_ROWS) for j in range(N_COLS) for  k in range(16)]
-pair_list = np.arange(N_ROWS * N_COLS, dtype=np.int64)
-pairs_array = np.array(pair_list)
-# print(len(pair_list))
-print(data_array.shape)
-
-start_time = time()
-result_array = data_array[pairs_array]
-
-
-
+# print("Taking pairs")
+# # pair_list = [(i * N_COLS + j + k) % N_ROWS for i in range(N_ROWS) for j in range(N_COLS) for  k in range(16)]
+# pair_list = np.arange(N_ROWS * N_COLS, dtype=np.int64)
+# pairs_array = np.array(pair_list)
+# # print(len(pair_list))
+# print(data_array.shape)
 
 # start_time = time()
-result_array_1 = data_array[pairs_array]
-result_array_2 = data_array[pairs_array]
-duration = time() - start_time
-print(duration / 1e6)
+# result_array = data_array[pairs_array]
+
+
+
+
+# # start_time = time()
+# result_array_1 = data_array[pairs_array]
+# result_array_2 = data_array[pairs_array]
+# duration = time() - start_time
+# print(duration / 1e6)
 
 # start_time = time()
 # result_array = data_array[pairs_array]
@@ -90,3 +90,37 @@ print(duration / 1e6)
 # print(result_array.shape)
 # print(result_array)
 # # print(duration / 1e6)
+
+
+########################################################
+########################################################
+########################################################
+# import cupy as np
+
+import cupynumeric as np
+from legate.timing import time
+
+
+N_ROWS = 1_000_000
+N_COLS = 32
+
+
+def take_with_pairs(data, pairs):
+    return np.take(data, pairs, axis=1)
+
+
+data_array = np.arange(N_ROWS * N_COLS, dtype=np.float32).reshape((N_ROWS, N_COLS))
+print("Data generated")
+
+print("Taking pairs")
+pair_list = [[i, j] for i in range(N_COLS) for j in range(i + 1, N_COLS)]
+pairs_array = np.array(pair_list)
+print(len(pair_list))
+
+start_time = time()
+result_array = take_with_pairs(data_array, pairs_array)
+duration = time() - start_time
+
+print(result_array.shape)
+
+print(duration / 1e6)

a41c7241a9fd576935aca8bbd5655c13a9c3934e
Shixun Wu <shixunw@lego-c2-qs-60.cm.cluster>
2025-08-25 16:03:53 -0700

    update

--- src/cupynumeric/all2all/all2all.cu
+++ src/cupynumeric/all2all/all2all.cu
@@ -251,8 +251,8 @@ void global_all2all(
   
   cudaStreamSynchronize(stream);
     thrust::exclusive_scan(DEFAULT_POLICY.on(stream), 
-                          round1_send_histo.ptr(0), 
-                          round1_send_histo.ptr(0) + num_ranks, round1_send_offsets.ptr(0));
+                          (unsigned int*)round1_send_histo.ptr(0), 
+                          ((unsigned int*)round1_send_histo.ptr(0)) + num_ranks, round1_send_offsets.ptr(0));
   cudaDeviceSynchronize();
   
 

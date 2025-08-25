5e797a30628ff464c74e43352ff4c2830f57a64a
Shixun Wu <shixunw@lego-c2-qs-60.cm.cluster>
2025-08-25 15:38:25 -0700

    update

--- src/cupynumeric/all2all/all2all.cu
+++ src/cupynumeric/all2all/all2all.cu
@@ -261,18 +261,18 @@ void global_all2all(
     CHECK_NCCL(ncclRecv((void*)round1_recv_histo.ptr(i), 1, ncclUint32, i, *nccl_comm, stream));  
     CHECK_NCCL(ncclSend((void*)round1_send_histo.ptr(i), 1, ncclUint32, i, *nccl_comm, stream));
   }
-  CHECK_NCCL(ncclGroupEnd());
-  size_t total_indices_to_receive = thrust::reduce(round1_recv_histo.ptr(0), round1_recv_histo.ptr(0) + num_ranks);
-  cudaStreamSynchronize(stream);
-  // Pack request indices by target rank
+  // CHECK_NCCL(ncclGroupEnd());
+  // size_t total_indices_to_receive = thrust::reduce(round1_recv_histo.ptr(0), round1_recv_histo.ptr(0) + num_ranks);
+  // cudaStreamSynchronize(stream);
+  // // Pack request indices by target rank
   
-  pack_request_indices_kernel<DIM_input><<<grid_size, block_size, 0, stream>>>(index_ptr, num_requests,  
-    (legate::Point<DIM_input>*)(round2_send_indices.ptr(0)), 
-    round1_send_offsets.ptr(0),
-    round2_request_positions.ptr(0), 
-    packing_counters.ptr(0), 
-    (legate::Rect<DIM_input>*)(global_rects.ptr(0)), num_ranks);
-  cudaStreamSynchronize(stream);
+  // pack_request_indices_kernel<DIM_input><<<grid_size, block_size, 0, stream>>>(index_ptr, num_requests,  
+  //   (legate::Point<DIM_input>*)(round2_send_indices.ptr(0)), 
+  //   round1_send_offsets.ptr(0),
+  //   round2_request_positions.ptr(0), 
+  //   packing_counters.ptr(0), 
+  //   (legate::Rect<DIM_input>*)(global_rects.ptr(0)), num_ranks);
+  // cudaStreamSynchronize(stream);
     
 
   // auto round2_recv_indices = create_buffer<legate::Point<DIM_input>>(total_indices_to_receive, Memory::Kind::GPU_FB_MEM);

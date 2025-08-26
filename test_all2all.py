1cdcf6cbf74ece0827fc4cf9cda1ce36a4dfd9de
Shixun Wu <shixunw@lego-c2-qs-60.cm.cluster>
2025-08-26 07:13:11 -0700

    update

--- test_all2all.py
+++ test_all2all.py
@@ -5,7 +5,7 @@ import numpy
 input = numpy.arange(8000000)
 # input = input.reshape(10, 10)
 
-a = np.array(input)
+a = np.array(input, dtype=np.float32)
 print(a)
 # b = a[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]]
 b = a[[0,1,2,3]]

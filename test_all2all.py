c671f55c35ad3fcf38c271a0a9f5a83eab18321f
Shixun Wu <shixunw@lego-c2-qs-60.cm.cluster>
2025-08-26 07:15:20 -0700

    update

--- test_all2all.py
+++ test_all2all.py
@@ -2,7 +2,7 @@ import cupynumeric as np
 import numpy
 
 # input = numpy.array([0,1,2,3,4,5,6,7,8,9])
-input = numpy.arange(32)
+input = numpy.arange(8000000)
 # input = input.reshape(10, 10)
 
 a = np.array(input, dtype=np.float32)

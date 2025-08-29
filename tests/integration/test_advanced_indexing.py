aa0c5e63f43fb809bfbfe715b62618864036a7d9
Shixun Wu <shixunw@lego-c2-qs-60.cm.cluster>
2025-08-29 14:25:15 -0700

    update

--- tests/integration/test_advanced_indexing.py
+++ tests/integration/test_advanced_indexing.py
@@ -66,6 +66,8 @@ VALS_EMPTY_1D = (num.array([]),)
 @pytest.mark.parametrize("arr", ARRS_FIXTURES)  # arr = [42]
 def test_getitem_scalar_0d(arr, idx, request):
     arr = request.getfixturevalue(arr)
+    print(arr)
+    print(idx)
     assert np.array_equal(arr[idx], 42)
 
 

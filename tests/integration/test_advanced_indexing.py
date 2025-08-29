90608a44c8b5e6451d1508287f1e41d75da9e309
Shixun Wu <shixunw@lego-c2-qs-60.cm.cluster>
2025-08-29 14:26:16 -0700

    update

--- tests/integration/test_advanced_indexing.py
+++ tests/integration/test_advanced_indexing.py
@@ -68,7 +68,6 @@ def test_getitem_scalar_0d(arr, idx, request):
     arr = request.getfixturevalue(arr)
     print(arr)
     print(idx)
-    assert False
     assert np.array_equal(arr[idx], 42)
 
 

25a644f6731c060e43c1ad4cd14939e883e9daeb
Shixun Wu <shixunw@lego-c2-qs-60.cm.cluster>
2025-09-03 13:59:37 -0700

    update

--- run_2.sh
+++ run_2.sh
@@ -33,7 +33,7 @@ export GASNET_IBV_PORTS=mlx5_0+mlx5_3+mlx5_4+mlx5_5+mlx5_6+mlx5_9+mlx5_10+mlx5_1
 # export REALM_BACKTRACE=1
 # export LEGION_BACKTRACE=1
 
-export OMPI_MCA_btl_base_verbose=100  # 最详细的 BTL 调试
+# export OMPI_MCA_btl_base_verbose=100  # 最详细的 BTL 调试
 export HOME=/project/coreai_devtech_all/shixunw;
 export MPI_HOME=$HPC_SDK_ROOT/comm_libs/12.8/hpcx/hpcx-2.22.1/ompi;
 export HPC_SDK_ROOT=/opt/nvidia/hpc_sdk/Linux_x86_64/25.3;
@@ -62,8 +62,8 @@ export PATH=$MPI_HOME/bin:$PATH;
 export MPI_C_COMPILER=mpicc;
 export MPI_CXX_COMPILER=mpicxx;
 export ucc_DIR=$HPC_SDK_ROOT/comm_libs/12.8/hpcx/hpcx-2.22.1/ucc/lib/cmake/ucc;
-export UCX_LOG_LEVEL=error;
-export UCX_MODULE_LOG_LEVEL=error;
+# export UCX_LOG_LEVEL=error;
+# export UCX_MODULE_LOG_LEVEL=error;
 export NCCL_DEBUG=INFO
 
 
